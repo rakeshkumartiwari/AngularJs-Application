b0a7885be826d07cb064885403bec7726f0f3893
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-03-25 00:26:31 +0530

    AngularJs-application

---
+++ AngularJs/Count Likes and Dislikes/script.js
@@ -0,0 +1,16 @@
+﻿var myApp = angular.module("myModule", [])
+    .controller("myController", function($scope) {
+        var technologies = [
+            { Name: "C#", Likes: 0, Dislikes: 0 },
+            { Name: "JavaScript", Likes: 0, Dislikes: 0 },
+            { Name: "Asp.Net", Likes: 0, Dislikes: 0 }
+
+        ];
+        $scope.technologies = technologies;
+    $scope.increamentLikes = function(technology) {
+        technology.Likes++;
+    }
+    $scope.increamentDislikes = function (technology) {
+        technology.Dislikes++;
+    }
+});
---
+++ AngularJs/Search Filter in AngularJs/script.js
@@ -0,0 +1,12 @@
+﻿var myApp = angular.module("myModule", [])
+                   .controller("myController",function($scope) {
+                       var employees = [
+                           { Name: "Ben Jule", Gender: "Male", Salary: 25000, City: "London" },
+                           { Name: "Julie Parse", Gender: "Female", Salary: 65000, City: "USA" },
+                           { Name: "Sara Mark", Gender: "Female", Salary: 89000, City: "Hyderabad" },
+                           { Name: "Todd Nail", Gender: "Male", Salary: 78000, City: "London" },
+                           { Name: "Vanch Hump", Gender: "Male", Salary: 45000, City: "Chennai" }
+                       ];
+
+    $scope.employees = employees;
+});
